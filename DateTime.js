5bf771889abbcc2e629562fa6adb5b08d77124dd
halimgambit <70286759+halimgambit@users.noreply.github.com>
2023-04-08 08:51:37 +0200

    Update DateTime.js

--- DateTime.js
+++ DateTime.js
@@ -49,12 +49,8 @@ function time (data, client) {
 		Avatar.Speech.end(data.client);
 		});
 	}
-
 });
-
 })
-
-
 }
 
 
@@ -63,48 +59,7 @@ function date (data, client) {
 Avatar.speak("Nous somme le." + new Date().toLocaleDateString('fr-fr', {weekday:"long", year:"numeric", month:"short", day:"numeric"}) , data.client, function(){
 	Avatar.Speech.end(data.client);
 	});
-
 }
-
-	/*
-
-		fetch("https://www.timeapi.io/api/Time/current/zone?timeZone=Europe/Paris")
-	.then(response => response.json())
-	.then(response2 => {
-console.log(response2)
-	Avatar.speak("Nous somme le." + date, data.client, function(){
-		Avatar.Speech.end(data.client);
-	});
-	});
-})
-*/
-
-
-/*
-let annee = "2023";
-	var today = new Date();
-    annee_actuel = today.getFullYear();
-    mois_actuel = today.getMonth() +1;
-    jour_actuel = today.getDate();
-
-	Avatar.speak("Nous somme le." + date, data.client, function(){
-	if(annee_actuel === annee && mois_actuel === 1 && jour_actuel === 1) {
-	Avatar.speak("aujourd'hui c'est le nouvel", data.client, function(){
-	Avatar.speak("Bonne année!", data.client, function(){
-	Avatar.Speech.end(data.client);
-	});
-    });
-}
-	if(annee_actuel === annee && mois_actuel === 12 && jour_actuel === 25) {
-		Avatar.speak("aujourd'hui c'est noél!", data.client, function(){
-		Avatar.speak("Joyeux noél!", data.client, function(){
-		Avatar.Speech.end(data.client);
-		});
-	});
-	}
-
-    });
-    */
 
 function rappel (data, client) {
 
@@ -119,55 +74,3 @@ function setClient(data){
     client = data.action.setRoom;
     return client;
 }
-
-
-/*
-
-var birthday = new Date(1994, 12, 10);
-var copy = new Date();
-copy.setTime(birthday.getTime());
-console.log(copy);
-
-
-let end, start;
-
-let start = new Date();
-for (var i = 0; i < 3000; i++) {
- Math.sqrt(i);
-}
-let end = new Date();
-console.log("L'operation prend." + (end.getTime() - start.getTime()) + ' msec');
-
-
-var date = new Date();
-var heure = date.getHours();
-var minute = date.getMinutes();
-alert(heure + "heure:" + "et" + ":" + minute + "minute");
-
-
-
-var moment = require('moment');
-moment.lang('fr');
-var heure = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
-alert(heure);
-
-
-
-var now = new Date(); 
-var datetime = ' '+now.getHours()+':'+now.getMinutes()+':'+now.getSeconds();
-alert(datetime);
-
-
-var now = new Date(); 
-  var date = now.getFullYear()+'/'+(now.getMonth()+1)+'/'+now.getDate(); 
-  var time = ' '+now.getHours()+':'+now.getMinutes()+':'+now.getSeconds();
-  alert(date + "" + time);
-
-
-var now = new Date(); 
-  var date = now.getFullYear()+'/'+(now.getMonth()+1)+'/'+now.getDate(); 
-  alert(date);
-
-var d = new Date();
-alert(d.toLocaleString());
-*/
